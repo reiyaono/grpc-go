8dbbb124e39aa4ecf3b50401718dad56358d7dfa
reiyaono <reiya018018s@gmail.com>
2025-06-03 01:46:12 +0900

    develop server

---
+++ grpc-lesson/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc-lesson/pb"
+	"io/ioutil"
+)
+
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles called")
+
+	dir := "/Users/018018s/dev/go-grpc/grpc-lesson/storage"
+
+	paths, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, path := range paths {
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
+}
